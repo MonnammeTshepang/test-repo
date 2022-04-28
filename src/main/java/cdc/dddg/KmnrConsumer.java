fe01d5a89cbf3aaa46338945014a69f89d6640c6
Patrick Lackerbauer (ext.) <patrick.lackerbauer@partner.bmw.de>
2022-04-28 18:15:29 +0200

    Added consumer for multiple topics

--- src/main/java/cdc/dddg/KmnrConsumer.java
+++ src/main/java/cdc/dddg/KmnrConsumer.java
@@ -1,14 +1,25 @@
 package cdc.dddg;
 
-import io.smallrye.mutiny.Multi;
-import io.smallrye.reactive.messaging.kafka.Record;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
-import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.util.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 
 @ApplicationScoped public class KmnrConsumer {
+    String message;
 
+    @Incoming("kmnr")
+        public void consume(ConsumerRecord<String, String> record) {
+        String topic = record.topic();
+        String key = record.key();
+        String value = record.value();
+        long timestamp = record.timestamp();
+
+        this.message = record.toString();
+
+        System.out.println("timestamp: " + timestamp + ", topic: " + topic + ", value: " + value + "\n");
+    }
 }
