24ad45c0c751585ced4c8f4570a6b702634f760f
Bartosz Pazdziur <Bartosz.pa.Pazdziur@partner.bmw.de>
2022-05-18 10:16:37 +0200

    update kmnr mapper

--- src/main/java/cdc/dddg/KmnrMapper.java
+++ src/main/java/cdc/dddg/KmnrMapper.java
@@ -3,6 +3,7 @@ package cdc.dddg;
 import org.apache.avro.generic.GenericRecord;
 
 import java.time.Instant;
+import java.time.LocalDate;
 
 public class KmnrMapper {
     public KmnrMapper() {
@@ -17,9 +18,15 @@ public class KmnrMapper {
         kmnr.setKogr(genericRecord.get("kogr").toString());
         kmnr.setStand(genericRecord.get("stand").toString());
         kmnr.setKmabtlung(genericRecord.get("kmabtlung").toString());
-        //kmnr.setDtaen(genericRecord.get("dtaen").toString());
-        //kmnr.setDtanl(genericRecord.get("dtanl").toString());
-        //kmnr.setZielTermin(genericRecord.get("ziel_termin").toString());
+        if (genericRecord.get("dtaen") != null) {
+            kmnr.setDtaen(LocalDate.parse(genericRecord.get("dtaen").toString()));
+        }
+        if (genericRecord.get("dtanl") != null) {
+            kmnr.setDtanl(LocalDate.parse(genericRecord.get("dtanl").toString()));
+        }
+        if (genericRecord.get("ziel_termin") != null) {
+            kmnr.setZielTermin(LocalDate.parse(genericRecord.get("ziel_termin").toString()));
+        }
         if (genericRecord.get("ze_termin") != null) {
             kmnr.setZeTermin(genericRecord.get("ze_termin").toString());
         }
